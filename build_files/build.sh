0fa9f557637f7db0bb477b5f16f4be5ae1daf308
Jay Hemsley <jay@hemsley.dev>
2025-06-04 18:25:47 -0400

    lets try this

--- build_files/build.sh
+++ build_files/build.sh
@@ -15,6 +15,7 @@ source /ctx/build_files/steps/01-install-packages.sh
 source /ctx/build_files/steps/02-ui.sh
 source /ctx/build_files/steps/03-overrides.sh
 source /ctx/build_files/steps/04-final.sh
+source /ctx/build_files/steps/05-signing.sh
 
 rm -rf /tmp/* || true
 find /var/* -maxdepth 0 -type d \! -name cache -exec rm -fr {} \;
